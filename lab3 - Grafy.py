4a0b12b57825c5612f833b46fa4ec554473fd083
Unknown <wdziuba@student.agh.edu.pl>
2018-10-23 21:28:59 +0200

    lab3 - Grafy - pierwsza funkcja gotowa

---
+++ lab3 - Grafy.py
@@ -0,0 +1,20 @@
+import matplotlib
+import numpy
+
+
+def adjmat_to_adjlist(m: list)->list:
+    temp = []
+    print(temp)
+    for j in range(len(m)):
+        temp.append([])
+        for i in range(len(m)):
+            if m[j][i] is not 0:
+                for c in range(m[j][i]):
+                    temp[j].append(i+1)
+                    print("j:", j, "i:", i, "c:", c, " ", temp)
+    return temp
+
+
+matrix = [[0, 1, 0], [0, 0, 1], [1, 2, 0]]
+ans = adjmat_to_adjlist(matrix)
+print(ans)
